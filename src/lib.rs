fbf713a2f884d4debcd64ca927c287ee348cb825
Andrew Green <agreen@ebi.ac.uk>
2022-11-08 23:30:08 +0000

    Add main page documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+// #![warn(missing_docs)]
+//! Sequence features in your browser!
+//!
+//! Provides a selection of RNA appropriate sequence features
+//! which can be extracted to use for ML, or anything you like
+//!
+//! The library is intended to compile to WASM for client-side
+//! extraction, but there's nothing to stop you compiling for
+//! host execution if you want to!
+//!
+
 #[macro_use]
 extern crate lazy_static;
 
