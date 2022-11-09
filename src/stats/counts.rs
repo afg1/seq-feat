7dc630af44e0060b9364a471e7aace1063553882
Andrew Green <agreen@ebi.ac.uk>
2022-11-09 14:31:40 +0000

    Add some stub documentation for the counts module

--- src/stats/counts.rs
+++ src/stats/counts.rs
@@ -1,6 +1,11 @@
 use crate::utils::*;
 use wasm_bindgen::prelude::*;
 
+/// Get the fraction of the sequence that is either a G or C
+///
+/// GC content is a fairly well established feature.
+/// This function simply counts the ocurrences of each
+/// character and divides by the total sequence length.
 #[wasm_bindgen(js_name=gc_content)]
 pub fn gc_content(seq: &str) -> f64 {
     let clean_seq = seq::clean(seq);
@@ -10,26 +15,31 @@ pub fn gc_content(seq: &str) -> f64 {
     (num_c + num_g) / clean_seq.len() as f64
 }
 
+/// Calculate the number of A nucleotides in a sequence
 pub fn a_content(seq: &str) -> f64 {
     let clean_seq: String = seq::clean(seq);
     clean_seq.matches('A').count() as f64
 }
 
+/// Calculate the number of C nucleotides in a sequence
 pub fn c_content(seq: &str) -> f64 {
     let clean_seq: String = seq::clean(seq);
     clean_seq.matches('C').count() as f64
 }
 
+/// Calculate the number of G nucleotides in a sequence
 pub fn g_content(seq: &str) -> f64 {
     let clean_seq: String = seq::clean(seq);
     clean_seq.matches('G').count() as f64
 }
 
+/// Calculate the number of U nucleotides in a sequence
 pub fn u_content(seq: &str) -> f64 {
     let clean_seq: String = seq::clean(seq);
     clean_seq.matches('U').count() as f64
 }
 
+// Get nucleotide counts at given phase
 pub fn get_phased_counts(seq: &str, phase: usize) -> (f64, f64, f64, f64) {
     let phase_a: f64 = seq
         .chars()
